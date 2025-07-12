144b1bea3356c28637b0dab45e0e595148183e75
TechnogodCEO <rosha320neo@gmail.com>
2025-07-12 18:58:50 -0400

    just some reorganization of the java file

--- Snake.java
+++ Snake.java
@@ -1,11 +1,14 @@
 //import packages for graphics
 import java.awt.*;
 import javax.swing.*;
+
 //import event package
 import java.awt.event.*;
+
 //import logical packages
 import java.lang.Math;
 import java.util.ArrayList;
+
 public class Snake extends JPanel implements KeyListener, ActionListener
 {
 	
@@ -31,8 +34,11 @@ public class Snake extends JPanel implements KeyListener, ActionListener
 	  */
 	
    //create global variables
+	int tickspeed = 100; //controls tickspeed
+
 	ArrayList<Integer> x = new ArrayList<Integer>();
 	ArrayList<Integer> y = new ArrayList<Integer>();
+	
 	int length = 1;
 	
 	int applex = (int)((35) * Math.random());
@@ -50,7 +56,7 @@ public class Snake extends JPanel implements KeyListener, ActionListener
 	Font creditFont = new Font("Times New Roman", Font.PLAIN, 20);
 	
 	//create timer
-	Timer time = new Timer(100, this);
+	Timer time = new Timer(tickspeed, this);
 	
 	//__init__ Keyboard Listener
 	public Snake() {
@@ -64,121 +70,130 @@ public class Snake extends JPanel implements KeyListener, ActionListener
 	    y.add((int)((30) * Math.random()));
 	}
 	
+	/** Draws the gameboard while the game is still running**/
+	private void drawGameScreen(Graphics g) {
+		//draw title
+		g.setColor(Color.GREEN);
+		g.setFont(titleFont);
+		g.drawString("Snake", 210, 90);
+	
+		//draw score
+		g.setColor(Color.WHITE);
+		g.setFont(scoreFont);
+	
+		if (score < 10) {
+			g.drawString("Score: " + score, 600, 100);
+		} else {
+			g.drawString("Score: " + score, 550, 100);
+		}
+	
+		//draw copyright
+		g.setFont(creditFont);
+		g.drawString("Roshan Kareer © 2025", 550, 785);
+	
+		//draw borders
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 120, 740, 20);
+		g.fillRect(0, 740, 740, 20);
+		g.fillRect(0, 140, 20, 600);
+		g.fillRect(720, 140, 20, 600);
+	
+		//draw snake
+		g.setColor(Color.WHITE);
+	
+		for (int i = x.size() - 1; i >= x.size() - length; i--) {
+			g.fillRect((x.get(i)*20) + 20, (y.get(i)*20) + 140, 20, 20);
+		}
+	
+		//draw apple
+		g.setColor(Color.RED);
+		g.fillRect((applex*20) + 20, (appley*20) + 140, 20, 20);
+	
+		//run time event to wait until next step
+		time.start();
+	}
+
+	/** Draws the loss screen */
+	private void drawGameOverScreen(Graphics g) {
+		//draw GameOver:
+		g.setColor(Color.GREEN);
+		g.setFont(titleFont);
+		g.drawString("Game Over", 100, 350);
+	
+		//draw score
+		g.setFont(scoreFont);
+		g.drawString("Score: " + score, 310, 400);
+
+		//draw reset button;
+		g.setFont(selectFont);
+		g.drawString("Press R to restart", 260, 430);
+	
+		//draw copyright
+		g.setFont(creditFont);
+		g.drawString("Roshan Kareer © 2025", 550, 785);
+	}
+	
 	//draw graphics
 	public void paintComponent(Graphics g)
     {
-   	 //set up new game
-        super.paintComponent(g);
-        setBackground(Color.BLACK);
-       
-        //while the game is not over (like a loop)
+   	 	//set up new gameboard 
+        super.paintComponent(g); //clear
+        setBackground(Color.BLACK); //set bg to black
+		
+        // runs while the game is not over (like a loop)
         if(!gameover) {
-	         //draw title
-	         g.setColor(Color.GREEN);
-	         g.setFont(titleFont);
-	         g.drawString("Snake", 210, 90);
-	        
-	         //draw score
-	         g.setColor(Color.WHITE);
-	         g.setFont(scoreFont);
-	        
-	         if (score < 10) {
-	        	 g.drawString("Score: " + score, 600, 100);
-	         } else {
-	        	 g.drawString("Score: " + score, 550, 100);
-	         }
-	        
-	         //draw copyright
-	         g.setFont(creditFont);
-	         g.drawString("Roshan Kareer © 2025", 550, 785);
-	        
-	         //draw borders
-	         g.setColor(Color.GREEN);
-	         g.fillRect(0, 120, 740, 20);
-	         g.fillRect(0, 740, 740, 20);
-	         g.fillRect(0, 140, 20, 600);
-	         g.fillRect(720, 140, 20, 600);
-	        
-	         //draw snake
-	         g.setColor(Color.WHITE);
-	        
-	         for (int i = x.size() - 1; i >= x.size() - length; i--) {
-	        	 g.fillRect((x.get(i)*20) + 20, (y.get(i)*20) + 140, 20, 20);
-	         }
-	        
-	         //draw apple
-	         g.setColor(Color.RED);
-	         g.fillRect((applex*20) + 20, (appley*20) + 140, 20, 20);
-	        
-	         //run time event to wait until next step
-	         time.start();
-	        
+	        drawGameScreen(g);
         }
        
-        else // if game is over
-        {
-       	 //draw GameOver:
-       	 g.setColor(Color.GREEN);
-       	 g.setFont(titleFont);
-       	 g.drawString("Game Over", 100, 350);
-       	
-       	 //draw score
-       	 g.setFont(scoreFont);
-       	 g.drawString("Score: " + score, 310, 400);
-
-		 //draw reset button;
-		 g.setFont(selectFont);
-		 g.drawString("Press R to restart", 260, 430);
-       	
-       	 //draw copyright
-		 g.setFont(creditFont);
-       	 g.drawString("Roshan Kareer © 2025", 550, 785);
+        else /* if the game is over */ {
+			drawGameOverScreen(g);
         }
     }
 	
 	 @Override
 	 public void actionPerformed(ActionEvent e) {
-		 //check what the current direction is and move in that direction by adding a new square to that list
-		 int currx = x.get(x.size() - 1);
-		 int curry = y.get(y.size() - 1); //these two are the current heads
+		//check what the current direction is and move in that direction by adding a new square to that list
+		int currx = x.get(x.size() - 1);
+		int curry = y.get(y.size() - 1); //these two are the current heads
+	
+		if (direction.equals("right")) {
+			x.add(currx + 1);
+			y.add(curry);
+		} else if (direction.equals("up")) {
+			y.add(curry - 1);
+			x.add(currx);
+		} else if (direction.equals("left")) {
+			x.add(currx - 1);
+			y.add(curry);
+		} else if (direction.equals("down")) {
+			y.add(curry + 1);
+			x.add(currx);
+		}
+	
+		// check if there is a collision with the apple
+		if (applex == x.get(x.size() - 1) && appley == y.get(y.size() - 1)) {
+			score++; //add to the score
 		
-		 if (direction.equals("right")) {
-			 x.add(currx + 1);
-			 y.add(curry);
-		 } else if (direction.equals("up")) {
-			 y.add(curry - 1);
-			 x.add(currx);
-		 } else if (direction.equals("left")) {
-			 x.add(currx - 1);
-			 y.add(curry);
-		 } else if (direction.equals("down")) {
-			 y.add(curry + 1);
-			 x.add(currx);
-		 }
+			//respawn apple in a different location
+			setAppleRandomCoords();
 		
-		 // check if there is a collision with the apple
-		 if (applex == x.get(x.size() - 1) && appley == y.get(y.size() - 1)) {
-			 score++; //add to the score
-			
-			 //respawn apple in a different location
-			 setAppleRandomCoords();
-			
-			 //add to snake length
-			 length++;
-		 }
-		
-		 //collision detection with walls
-		 if (x.get(x.size() - 1) < 0 || x.get(x.size() - 1) > 34 || y.get(y.size() - 1) < 0 || y.get(y.size() - 1) > 29) {
-			 gameover = true;
-		 }
-		
-		 //check for self collision
+			//add to snake length
+			length++;
+		}
+	
+		//collision detection with walls
+		if (x.get(x.size() - 1) < 0 || x.get(x.size() - 1) > 34 || y.get(y.size() - 1) < 0 || y.get(y.size() - 1) > 29) {
+			gameover = true;
+		}
+	
+		//check for self collision
 		for (int i = x.size() - 3; i >= x.size() - length; i--) {
-       	 if(x.get(x.size() - 1) == x.get(i) && y.get(y.size() - 1) == y.get(i)) {
-       		 gameover = true;
-       	 }
+			if(x.get(x.size() - 1) == x.get(i) && y.get(y.size() - 1) == y.get(i)) {
+				gameover = true;
+			}
         }
-		 //reset the game board
+
+		//reset the game board
 		repaint();
 	 }
 	
@@ -204,7 +219,7 @@ public class Snake extends JPanel implements KeyListener, ActionListener
 
 	 @Override
 	 public void keyPressed(KeyEvent e) {
-		 if (!gameover) {
+		if (!gameover) {
 			// if the game isn't over, then whenever a key is pressed assign the correct direction based on the key pressed
 			// the && direction part makes it so you don't instantly kill yourself if you press the button in the opposite direction of movement. 
 			// () are needed around the different key options as &&nds execute before ||rs.
@@ -217,15 +232,15 @@ public class Snake extends JPanel implements KeyListener, ActionListener
 		 	} else if ((e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) && direction != "left") {
 			 	direction = "right"; // D or right is right
 		 	}
-		 } 
+		} 
 		 
-		 else // if the game is over
-		 {
-			//add an option to reset
-			if(e.getKeyCode() == KeyEvent.VK_R) {
-				reset();
-			}
-		 }
+		else // if the game is over
+		{
+		//add an option to reset
+		if(e.getKeyCode() == KeyEvent.VK_R) {
+			reset();
+		}
+		}
 	 }
 	 
 	//this method resets the game without having to restart the app
